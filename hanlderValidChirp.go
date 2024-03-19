141febd15d1576cb88fc113a6ddf938ecd79b1fb
Iyed-M <iyedmhiri01@gmail.com>
2024-03-19 20:31:00 +0100

    improve error handling and response

--- hanlderValidChirp.go
+++ hanlderValidChirp.go
@@ -3,7 +3,6 @@ package main
 import (
 	"encoding/json"
 	"io"
-	"log"
 	"net/http"
 )
 
@@ -11,28 +10,49 @@ type chirpRequest struct {
 	Body string `json:"body"`
 }
 
-type errorResp struct {
-	Error string `json:"error"`
-}
-
 func handlerValidChirp() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		chirp := &chirpRequest{}
 		reqBody, err := io.ReadAll(r.Body)
 		if err != nil {
-			log.Print(err)
-		}
-		err = json.Unmarshal(reqBody, chirp)
-		if err != nil {
-			log.Print(err)
+			if err = RespondWithError(w, 400, "Something went wrong"); err != nil {
+				RespondWithError(w, 500, "Something went wrong")
+			}
 		}
 
-		if len(chrip.Body) > 140 {
-			invalidResp := &errorResp{""}
+		err = json.Unmarshal(reqBody, chirp)
+		if err != nil {
+			if err = RespondWithError(w, 400, "Something went wrong"); err != nil {
+				RespondWithError(w, 500, "Something went wrong")
+			}
+			return
+		}
+
+		if len(chirp.Body) > 140 {
+			if err = RespondWithError(w, 400, "Chirp is too long"); err != nil {
+				RespondWithError(w, 500, "Something went wrong")
+			}
+			return
+		}
+
+		if err = RespondWithJSON(w, 200, map[string]bool{"valid": true}); err != nil {
+			RespondWithError(w, 500, "Something went wrong")
 		}
 	})
 }
 
-func RespondWihtJSON(w http.ResponseWriter, r *http.Request) {
+func RespondWithJSON(w http.ResponseWriter, status int, data interface{}) error {
+	response, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.WriteHeader(status)
+	w.Write(response)
+	return nil
+}
 
+func RespondWithError(w http.ResponseWriter, status int, message string) error {
+	return RespondWithJSON(w, status, map[string]string{"error": message})
 }
