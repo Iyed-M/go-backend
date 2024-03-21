1d2958dd597094a9bf12228d25a8fc1252d75f90
Iyed-M <iyedmhiri01@gmail.com>
2024-03-21 21:47:28 +0100

    add password hashing

--- database/users.go
+++ database/users.go
@@ -5,18 +5,20 @@ import (
 )
 
 type User struct {
-	Email string `json:"email"`
-	ID    int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	ID       int    `json:"id"`
 }
 
 // CreateUser creates a new user from email , stores it and returns it
-func (db *DB) CreateUser(email string) (User, error) {
+func (db *DB) CreateUser(email string, password string) (User, error) {
 	db.mu.Lock()
 	defer db.mu.Unlock()
 
 	usr := User{
-		ID:    db.getLastUserID(),
-		Email: email,
+		ID:       db.getLastUserID(),
+		Email:    email,
+		Password: password,
 	}
 
 	dbStruct, err := db.loadDB()
--- handlerPostUsers.go
+++ handlerPostUsers.go
@@ -6,14 +6,22 @@ import (
 	"net/http"
 
 	"github.com/Iyed-M/go-backend/utils"
+	"golang.org/x/crypto/bcrypt"
 )
+
+type postUserReq struct {
+	Password string `json:"password"`
+	Email    string `json:"email"`
+}
 
 type postUserResp struct {
 	Email string `json:"email"`
+	ID    int    `json:"id"`
 }
 
 func (cfg *apiConfig) handlerPostUsers() http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Read Request Body
 		resp, err := io.ReadAll(r.Body)
 		if err != nil {
 			utils.RespondWithError(w, http.StatusInternalServerError, "error reading request body")
@@ -21,19 +29,35 @@ func (cfg *apiConfig) handlerPostUsers() http.Handler {
 		}
 		r.Body.Close()
 
-		parsedEmail := postUserResp{}
-		err = json.Unmarshal(resp, &parsedEmail)
+		// Parse Request Body
+		parsedReq := postUserReq{}
+
+		err = json.Unmarshal(resp, &parsedReq)
 		if err != nil {
 			utils.RespondWithError(w, http.StatusInternalServerError, "error parsing request body")
 			return
 		}
 
-		usr, err := cfg.db.CreateUser(parsedEmail.Email)
+		// create user save it to db and retrun it
+		hashedPassord, err := bcrypt.GenerateFromPassword([]byte(parsedReq.Password), bcrypt.DefaultCost)
+		if err != nil {
+			utils.RespondWithError(w, 500, "Something went wrong")
+		}
+		// User data stoted in db
+		// the password is returned and hashed
+		usr_, err := cfg.db.CreateUser(parsedReq.Email, string(hashedPassord))
+
+		// POST RESPONSE
+		usrResp := postUserResp{
+			Email: usr_.Email,
+			ID:    usr_.ID,
+		}
+
 		if err != nil && err.Error() != "empty file" {
 			utils.RespondWithError(w, http.StatusInternalServerError, "error creating user")
 			return
 		}
 
-		utils.RespondWithJSON(w, 201, usr)
+		utils.RespondWithJSON(w, 201, usrResp)
 	})
 }
